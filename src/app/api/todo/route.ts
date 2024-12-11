af685d44e18b931885daf1c8786256bbe242ce57
Adnankhan10s <97806852+Adnankhan10s@users.noreply.github.com>
2024-12-11 06:17:33 -0800

    apis

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 
-export async function POST(req:NextRequest , res:NextResponse) {
+export async function POST(req:NextRequest) {
       try {
         const body = await req.json();
         await DbConnect();
@@ -20,7 +20,7 @@ export async function POST(req:NextRequest , res:NextResponse) {
       }
 }
 
-export async function GET(req:NextRequest , res:NextResponse) {
+export async function GET(req:NextRequest) {
     try {
         await DbConnect();
         const todos= await Todo.find({});
