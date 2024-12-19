62b67eda52995ee87280228506e08ac2299be4c3
Adnankhan10s <97806852+Adnankhan10s@users.noreply.github.com>
2024-12-19 05:52:25 -0800

    add edit

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -5,9 +5,9 @@ import {  NextResponse } from "next/server";
 
 
 
-export async function PUT (req:Request,{params}:{params:{id:string}}) {
+export async function PUT (req:Request, context: { params: { id: string } }) {
   await DbConnect();
-  const {id} =  params
+  const {id} =  context.params;
   const {title , description} = await req.json();
   try {
     const updatedTodo = await Todo.findByIdAndUpdate(id,
@@ -19,9 +19,9 @@ export async function PUT (req:Request,{params}:{params:{id:string}}) {
   }
   
 }
-export async function GET(request: Request , { params }:{params:{id:string}}) {
+export async function GET(request: Request ,  context: { params: { id: string } }) {
   try {
-    const {id} =  params;
+    const {id} = context.params;
     await DbConnect();
     const todo = await Todo.findById(id);
     
@@ -33,9 +33,9 @@ export async function GET(request: Request , { params }:{params:{id:string}}) {
     return NextResponse.json({ error: "Error fetching todo" }, { status: 500 });
   }
 }
-export async function DELETE(req:Request,{params}:{params:{id:string}}) {
+export async function DELETE(req:Request, context: { params: { id: string } }) {
   await DbConnect();
-  const {id} =  params
+  const {id} = context.params
   try {
     await Todo.findByIdAndDelete(id)
     return NextResponse.json({message:"Deleted successfully"},{status:201})
